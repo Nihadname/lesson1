1e9cee55a0b2863a3e2cc2fef618d32fce8821d1
Nihadname <nihadcoding@gmail.com>
2023-10-08 00:13:21 +0400

    Add project files.

---
+++ lesson1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num1 = 9;
+int num2 = 7;
+
+(num1, num2) = (num2, num1);
+
+Console.WriteLine(num1); 
+Console.WriteLine(num2); 
