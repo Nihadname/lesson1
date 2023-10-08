0b39c2029586e9763b3df9aa7baf8129ebd1f8d5
Nihadname <nihadcoding@gmail.com>
2023-10-08 17:30:30 +0400

    it is finished

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,9 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int num1 = 9;
+int num1 = 6;
 int num2 = 7;
-
-(num1, num2) = (num2, num1);
-
-Console.WriteLine(num1); 
-Console.WriteLine(num2); 
+Console.wri
